b900be3e9c6a990d9e124b3debc8ace5958ae657
johnmichaellim041202 <johnmichaellim041202@gmail.com>
2024-11-04 16:00:26 +0800

    chap 38 finished

--- locators.py
+++ locators.py
@@ -13,7 +13,11 @@ import time
 service_obj = Service("/Users/NCTV_User_002/Documents/chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
 
+#The website to use
 driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+#Maximize to fullscreen
+driver.maximize_window()
 
 # ID, Xpath, CSSSelector, Classname, name, linkText
 # By Name > name
@@ -22,8 +26,31 @@ driver.find_element(By.NAME, "name").send_keys("Baguette")
 driver.find_element(By.NAME, "email").send_keys("kys@gmail.com")
 # By ID > password
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("password")
-# By ID > clickCheckbutton to see password
+# By ID > clickCheckbutton to check the checkBox
 driver.find_element(By.ID, "exampleCheck1").click()
+# By CSS SELECTOR
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+time.sleep(4)
 
+# to scroll down the webpage
+driver.execute_script('window.scrollBy(0, 1000)')
+
+# XPATH = //tagname[@attributes='value'] -> //input[@type='submit']
+# CSS = tagname[@attributes='value'] -> //input[@type='submit']
+# By XPATH > //input[@value='Submit'] to click the Submit button
+driver.find_element(By.XPATH, "//input[@value='Submit']").click()
+
+# By XPATH > Two Way Data Binding example
+driver.find_element(By.XPATH, "//input[@class='ng-untouched ng-pristine ng-valid']").send_keys(" ayaw sa")
+
+# Clear but specified the value = (//tagname[@attribute='value'])[indexes]
+driver.find_element(By.CSS_SELECTOR, "(//input[@type='text'])[3]").clear()
+time.sleep(5)
+
+
+# By CLASS NAME > alert-success to see the message prompt
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+#print the message if its success
+print(message)
 
 time.sleep(5)
